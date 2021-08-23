16314779770993e180a159db673d2d16dc3c7b91
eld3niz <LoremIpsum1.61>
2021-08-23 10:14:54 +0200

    Canvas for GUI Main

--- Inhalte/GUI Windows/GUI-Main.py
+++ Inhalte/GUI Windows/GUI-Main.py
@@ -1,1 +1,18 @@
 #Hier kommt der Code für das Hauptfenster rein
+
+import tkinter as tk
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan = 3)
+
+#select Image for application
+#logo = Image.open()
+#logo = ImageTk.PhotoImage(logo)
+#logo_label = tk.Label(image=logo)
+#logo_label.image = logo
+#logo_label.grid(column=1, row = 0)
+
+root.mainloop()

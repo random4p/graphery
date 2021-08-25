935a611ca3de4c0ac790d8b40597dfcd8d363365
random4p <88232712+random4p@users.noreply.github.com>
2021-08-25 10:03:50 +0200

    ask for name window, still not working

--- Inhalte/GUI_Windows/GUI_Start.py
+++ Inhalte/GUI_Windows/GUI_Start.py
@@ -1,9 +1,9 @@
 # open and import window
 
 # imports
-from tkinter import Tk, Button, PhotoImage, Canvas, Label, filedialog
+from tkinter import Tk, Button, PhotoImage, Canvas, Label, filedialog, Entry
 import Data_Manager
-import GUI_Main
+
 
 # constants
 WINDOW_WIDTH = 750
@@ -14,21 +14,50 @@ LIGHT_GREEN = "#A7C4BC"
 LIGHT_LIGHT_GREEN = "#DFEEEA"
 
 
-# ----------------------------METHODS----------------------------------#
+# --------------------------GET_Name_WINDOW---------------------------------#
+class ProjectNameWindow(Tk):
+    def close_window(self):
+        self.name = self.project_name.get()
+        self.destroy()
+
+    def __init__(self):
+        super().__init__()
+        self.name = "*"
+        self.config(padx=50, pady=30, bg=LIGHT_LIGHT_GREEN)
+        self.title("Project Name")
+        self.geometry(
+            f"580x280+{int((self.winfo_screenwidth() / 2) - (WINDOW_WIDTH / 2))}+{int((self.winfo_screenheight() / 2) - (WINDOW_HEIGHT / 2))}")
+        label_qst = Label(self, text="How do you want to name your Project?",
+                          font=["Helvetica", "18", "bold"], fg=DARK_GREEN)
+        label_qst.grid(row=0, column=0, pady=10)
+        self.project_name = Entry(self, width=30)
+        self.project_name.grid(row=1, column=0, pady=20)
+        btn_close = Button(self, text="Okay", command=self.close_window, bg=GREEN, fg="White",
+                           width=20, pady=20, padx=20,
+                           font=("Arial", "18"), highlightthickness=0,
+                           borderwidth=0)
+        btn_close.grid(row=2, column=0, pady=10)
+
+
+# ----------------------------Import_Function----------------------------------#
 
 def import_file():
+    # get file from user
     file = filedialog.askopenfile(
         title='Open a file')
     file_type = file.name.split(".")[-1]
 
-    data_set = Data_Manager.DataManager(file, file_type)
+    # ask for project name
+    name_window = ProjectNameWindow()
+
+
+    # make our data set structured --> DataManager
+    data_set = Data_Manager.DataManager(file, file_type, name_window.name)
+    print(data_set.name)
 
     app.destroy()
-    main_window = GUI_Main.Window()
-    main_window.start()
 
-
-# ----------------------------WINDOW-----------------------------------#
+# ----------------------------Start_WINDOW-----------------------------------#
 # main class: create the start-window
 class StartWindow(Tk):
     def __init__(self):
@@ -69,3 +98,5 @@ class StartWindow(Tk):
 
 app = StartWindow()
 app.mainloop()
+
+

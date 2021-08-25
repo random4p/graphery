33ddd21b0e773e5c5aa9f258d38c1cbf058d4239
random4p <88232712+random4p@users.noreply.github.com>
2021-08-25 12:36:07 +0200

    added name field in gui start

--- Inhalte/GUI_Windows/GUI_Start.py
+++ Inhalte/GUI_Windows/GUI_Start.py
@@ -4,58 +4,30 @@
 from tkinter import Tk, Button, PhotoImage, Canvas, Label, filedialog, Entry
 import Data_Manager
 
-
 # constants
 WINDOW_WIDTH = 750
-WINDOW_HEIGHT = 400
+WINDOW_HEIGHT = 420
 DARK_GREEN = "#2F5D62"
 GREEN = "#5E8B7E"
 LIGHT_GREEN = "#A7C4BC"
 LIGHT_LIGHT_GREEN = "#DFEEEA"
 
 
-# --------------------------GET_Name_WINDOW---------------------------------#
-class ProjectNameWindow(Tk):
-    def close_window(self):
-        self.name = self.project_name.get()
-        self.destroy()
-
-    def __init__(self):
-        super().__init__()
-        self.name = "*"
-        self.config(padx=50, pady=30, bg=LIGHT_LIGHT_GREEN)
-        self.title("Project Name")
-        self.geometry(
-            f"580x280+{int((self.winfo_screenwidth() / 2) - (WINDOW_WIDTH / 2))}+{int((self.winfo_screenheight() / 2) - (WINDOW_HEIGHT / 2))}")
-        label_qst = Label(self, text="How do you want to name your Project?",
-                          font=["Helvetica", "18", "bold"], fg=DARK_GREEN)
-        label_qst.grid(row=0, column=0, pady=10)
-        self.project_name = Entry(self, width=30)
-        self.project_name.grid(row=1, column=0, pady=20)
-        btn_close = Button(self, text="Okay", command=self.close_window, bg=GREEN, fg="White",
-                           width=20, pady=20, padx=20,
-                           font=("Arial", "18"), highlightthickness=0,
-                           borderwidth=0)
-        btn_close.grid(row=2, column=0, pady=10)
-
-
 # ----------------------------Import_Function----------------------------------#
+
+# missing that the user can only open or import a file if he has entered a name in the entry field !!!
 
 def import_file():
     # get file from user
     file = filedialog.askopenfile(
         title='Open a file')
     file_type = file.name.split(".")[-1]
-
-    # ask for project name
-    name_window = ProjectNameWindow()
-
+    name = app.entry_name.get()
 
     # make our data set structured --> DataManager
-    data_set = Data_Manager.DataManager(file, file_type, name_window.name)
-    print(data_set.name)
-
+    data_set = Data_Manager.DataManager(file, file_type, name)
     app.destroy()
+
 
 # ----------------------------Start_WINDOW-----------------------------------#
 # main class: create the start-window
@@ -80,6 +52,10 @@ class StartWindow(Tk):
                            anchor="center", width=300)
         canvas.grid(column=0, row=0, columnspan=2)
 
+        # Entry
+        self.entry_name = Entry(width=40)
+        self.entry_name.grid(column=1, row=1, pady=10, padx=10)
+
         # Buttons
         btn_open = Button(bg=GREEN, text="Open",
                           fg="White", width=20, pady=20, padx=20,
@@ -88,15 +64,15 @@ class StartWindow(Tk):
                             fg="White", width=20, pady=20, padx=20,
                             font=("Arial", "18"), highlightthickness=0, borderwidth=0,
                             command=import_file)
-        btn_import.grid(column=0, row=1)
-        btn_open.grid(column=1, row=1)
+        btn_import.grid(column=0, row=2)
+        btn_open.grid(column=1, row=2)
 
         # Labels
-        impressum = Label(text="© 2021 eld3niz and random4p", bg=GREEN, fg="White", padx=243)
-        impressum.grid(column=0, row=2, columnspan=2, pady=15)
+        project_name_label = Label(text="Enter the project name:", font=("Arial", 15, "bold"))
+        project_name_label.grid(row=1, column=0, pady=10, sticky="e")
+        impressum_label = Label(text="© 2021 eld3niz and random4p", bg=GREEN, fg="White", padx=243)
+        impressum_label.grid(column=0, row=3, columnspan=2, pady=15)
 
 
 app = StartWindow()
 app.mainloop()
-
-

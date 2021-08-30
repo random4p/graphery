eca268462b5ca958564c5139c94072cfb9153287
eld3niz <LoremIpsum1.61>
2021-08-30 10:36:23 +0200

    added some comments

--- Inhalte/GUI_Windows/Window_Elements/PlotTab.py
+++ Inhalte/GUI_Windows/Window_Elements/PlotTab.py
@@ -8,19 +8,23 @@ class ConfigurePlotTab(QWidget):
     def __init__(self, parent, parent_2):
         super(QWidget, self).__init__(parent)
 
-        # add layout to dashboard
+        # add layout to plot
         parent.layout = QGridLayout()
         parent.layout.maximumSize()
 
+        #create 2x2 matrix of layouts to be filled later with plot and settings
         dim_layout = QGridLayout()
         plot_layout = QGridLayout()
         specify_layout = QGridLayout()
         add_layout = QGridLayout()
-        #parent.addLayout(dim_layout)
+        
+        #configure dim_layout where the user will choose which data/ columns will be plotted
+
+        #plot_tab is adding the 4 different layouts
         parent.layout.addLayout(dim_layout, 0, 0)
         parent.layout.addLayout(plot_layout, 0, 1)
         parent.layout.addLayout(specify_layout, 1, 0)
         parent.layout.addLayout(add_layout, 1, 1)
-        #parent.layout.addWidget(dim_frame, 0, 0)
+        
 
         parent.setLayout(parent.layout)

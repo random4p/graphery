22e12dc766f4d0b7a355b9339d30211741c9855d
eld3niz <LoremIpsum1.61>
2021-08-30 10:30:33 +0200

    changed layout of plot_tab

--- Inhalte/GUI_Windows/Window_Elements/PlotTab.py
+++ Inhalte/GUI_Windows/Window_Elements/PlotTab.py
@@ -11,3 +11,16 @@ class ConfigurePlotTab(QWidget):
         # add layout to dashboard
         parent.layout = QGridLayout()
         parent.layout.maximumSize()
+
+        dim_layout = QGridLayout()
+        plot_layout = QGridLayout()
+        specify_layout = QGridLayout()
+        add_layout = QGridLayout()
+        #parent.addLayout(dim_layout)
+        parent.layout.addLayout(dim_layout, 0, 0)
+        parent.layout.addLayout(plot_layout, 0, 1)
+        parent.layout.addLayout(specify_layout, 1, 0)
+        parent.layout.addLayout(add_layout, 1, 1)
+        #parent.layout.addWidget(dim_frame, 0, 0)
+
+        parent.setLayout(parent.layout)

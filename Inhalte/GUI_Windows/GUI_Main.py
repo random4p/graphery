07e0e75f9eebd861539ad405b021bd5d10b17091
random4p <88232712+random4p@users.noreply.github.com>
2021-08-31 11:29:17 +0200

    added table and button in preptab

--- Inhalte/GUI_Windows/GUI_Main.py
+++ Inhalte/GUI_Windows/GUI_Main.py
@@ -1,17 +1,18 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-#from DashTab import DashTab
+# from DashTab import DashTab
 
 import Inhalte.GUI_Windows.Window_Elements.DashTab
 import Inhalte.GUI_Windows.Window_Elements.MenuBar
 import Inhalte.GUI_Windows.Window_Elements.DataPrep
 import Inhalte.GUI_Windows.Window_Elements.PlotTab
- 
-#from Window_Elements.StyleSheet import Style
+
+
+# from Window_Elements.StyleSheet import Style
 
 class Window(QMainWindow):
-    
+
     def __init__(self):
         super().__init__()
         self.title = 'Graphery'
@@ -23,25 +24,25 @@ class Window(QMainWindow):
         self.top = 10
         self.width = 640
         self.height = 400
+        self.showMaximized()
         Inhalte.GUI_Windows.Window_Elements.MenuBar.MakeMenuBar(self)
-        
 
-        #Style(self)
+        # Style(self)
 
         self.table_widget = MyTableWidget(self)
         self.setCentralWidget(self.table_widget)
-    
+
 
 class MyTableWidget(QWidget):
-    
+
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
-        
+
         # Initialize tab screen
         self.tabs = QTabWidget()
-        
-        #create Tabs
+
+        # create Tabs
         self.dash = QWidget()
         self.plot = QWidget()
         self.stat = QWidget()
@@ -49,18 +50,18 @@ class MyTableWidget(QWidget):
         self.data_prep = QWidget()
         self.tabs.resize(300, 200)
 
-        #configure different tabs
+        # configure different tabs
         Inhalte.GUI_Windows.Window_Elements.DashTab.ConfigureDashTab(self.dash, self.tabs)
         Inhalte.GUI_Windows.Window_Elements.PlotTab.ConfigurePlotTab(self.plot)
-        
-        # Add specific tabs to tab-layout
-        self.tabs.addTab(self.dash,"                    Dashboard                    ")
-        self.tabs.addTab(self.plot,"                    Plot                    ")
-        self.tabs.addTab(self.stat,"                    Statistics                    ")
-        self.tabs.addTab(self.info,"                    Info                    ")
-        self.tabs.addTab(self.data_prep,"                    Data Preparation                    ")
+        Inhalte.GUI_Windows.Window_Elements.DataPrep.DataPrep(self.data_prep, self.tabs)
 
-    
-        #dont delete
+        # Add specific tabs to tab-layout
+        self.tabs.addTab(self.dash, "                    Dashboard                    ")
+        self.tabs.addTab(self.plot, "                    Plot                    ")
+        self.tabs.addTab(self.stat, "                    Statistics                    ")
+        self.tabs.addTab(self.info, "                    Info                    ")
+        self.tabs.addTab(self.data_prep, "                    Data Preparation                    ")
+
+        # dont delete
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
--- Inhalte/GUI_Windows/Window_Elements/DataPrep.py
+++ Inhalte/GUI_Windows/Window_Elements/DataPrep.py
@@ -14,23 +14,43 @@ class DataPrep:
         self.data = read_csv(DATA_SAMPLE1)
         self.data_list = [self.data[i].values for i in data.keys()]
         self.parent = parent
-        self.parent.table_widget = QTableWidget()
+        self.parent.layout = QGridLayout()
 
+        # to get more information of the row data
+        row_specification_layout = QGridLayout()
+        specifcation_label = QLabel("Column-Specification", parent)
+        for i in range(len(self.data.keys())):
+            specification_combo_box = QComboBox()
+            specification_combo_box.setMinimumSize(350, 50)
+            row_specification_layout.addWidget(specification_combo_box, i+1, 0)
+
+        # button
+        filter_button = QPushButton("Filter")
+        substitution_button = QPushButton("Substitute")
+        apply_button = QPushButton("Apply")
+
+        # create table of data
+        self.parent.table_widget = QTableWidget()
         self.create_table()
 
-        self.parent.layout = QVBoxLayout()
-        self.parent.layout.addWidget(parent.table_widget)
+        # add to the layout
+        self.parent.layout.addWidget(self.parent.table_widget, 0, 0, 5, 1)
+        self.parent.layout.addLayout(row_specification_layout, 0, 1)
+        self.parent.layout.addWidget(filter_button, 1, 1)
+        self.parent.layout.addWidget(substitution_button, 2, 1)
+        self.parent.layout.addWidget(apply_button, 4, 1)
         self.parent.setLayout(parent.layout)
 
     def create_table(self):
         self.parent.table_widget.setRowCount(self.data.shape[0]+1)
         self.parent.table_widget.setColumnCount(self.data.shape[1])
-        for i in range(len(self.data.keys())):
-            self.parent.table_widget.setItem(0, i, QTableWidgetItem(self.data.keys()[i]))
+        self.parent.table_widget.setHorizontalHeaderLabels(self.data.keys())
         for i in range(self.data.shape[1]):
             for j in range(self.data.shape[0]):
-                self.parent.table_widget.setItem(j+1, i, QTableWidgetItem(f"{self.data_list[i][j]}"))
+                self.parent.table_widget.setItem(j, i, QTableWidgetItem(f"{self.data_list[i][j]}"))
 
+        for i in range(len(self.data.keys())):
+            self.parent.table_widget.setColumnWidth(i, 250)
 
 
 
